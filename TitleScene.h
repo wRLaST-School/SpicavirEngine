2d578b06c2c0da29ab0b36c134d2a393749c7707
Wrelf <elf@wrelf.net>
2022-08-26 10:46:37 +0900

    added title scene

--- TitleScene.h
+++ TitleScene.h
@@ -3,5 +3,14 @@
 class TitleScene :
     public IScene
 {
+public:
+    ~TitleScene() {};
+    void Init() override;
+    void Update() override;
+    void DrawBack() override;
+    void Draw3D() override;
+    void DrawSprite() override;
+
+private:
 };
 
