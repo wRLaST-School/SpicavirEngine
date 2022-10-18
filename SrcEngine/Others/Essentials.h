3e290e572528969b72d7db7dd959e6849e0d6cdd
Wrelf <elf@wrelf.net>
2022-10-18 10:21:37 +0900

    added precompiled header

--- SrcEngine/Others/Essentials.h
+++ SrcEngine/Others/Essentials.h
@@ -1,19 +1,5 @@
 #pragma once
-#include  <Windows.h>
-#include <d3d12.h>
-#include <dxgi1_6.h>
-//#include <DirectXMath.h>
-#include "SpMath.h"
-#include <d3dcompiler.h>
-#include <d3dx12.h>
-#include <DirectXTex.h>
-#include <map>
-
-#include <vector>
-#include <string>
-#include <array>
-#include <wrl.h>
-#include <stdexcept>
+#include "stdafx.h"
 
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
