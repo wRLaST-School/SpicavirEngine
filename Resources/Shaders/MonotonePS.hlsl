565fe19ec771fb654664604c531bd57146559a1c
Wrelf <elf@wrelf.net>
2023-02-28 21:22:13 +0900

    適用されてなかったので再度

--- Resources/Shaders/MonotonePS.hlsl
+++ Resources/Shaders/MonotonePS.hlsl
@@ -6,9 +6,7 @@ SamplerState smp : register(s0);
 float4 main(VSOutput input) : SV_TARGET
 {
     float4 color = tex.Sample(smp, input.uv);
-    
-    float4 t = dot(color.xyz, float3(0.2125f, 0.7154f, 0.0721f));
-    clip(t - 1.0f);
+    color.xyz = color.xyz * float3(0.3f, 0.6f, 0.1f);
     
     return color;
 }
