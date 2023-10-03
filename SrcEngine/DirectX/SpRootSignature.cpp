27604057c7d6c9228a3b55ce7e7efd12d15e4429
wRLaST-School <103230447+wRLaST-School@users.noreply.github.com>
2023-10-03 12:50:15 +0900

    コンピュートシェーダーのサンプルを見ながらポチポチ

--- SrcEngine/DirectX/SpRootSignature.cpp
+++ SrcEngine/DirectX/SpRootSignature.cpp
@@ -7,7 +7,6 @@ std::map<std::string, SpRootSignature> SpRootSignature::sRsMap;
 
 void SpRootSignature::Create()
 {
-
 	D3D12_ROOT_SIGNATURE_DESC rootSignatureDesc{};
 	rootSignatureDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
 	rootSignatureDesc.pParameters = params.begin()._Ptr;
