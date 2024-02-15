4f5fe1a4317b57f1534cdbceb8962d7e83d75033
Wrelf <elf@wrelf.net>
2024-02-15 11:51:29 +0900

    AST用に各表現に対応するクラスをまとめて作成

---
+++ Libra/LibraCompiler/ExprAST.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <string>
+#include <vector>
+
+namespace Libra {
+	class ExprAST
+	{
+	public:
+		virtual ~ExprAST() {};
+	};
+
+	//数値を参照する式クラス(一旦全部数値はdoubleで扱う)
+	class NumberExprAST : public ExprAST {
+	private:
+		double val_;
+
+	public:
+		NumberExprAST(double val) : val_(val) {}
+	};
+
+	//変数を参照するための式クラス
+	class VariableExprAST : public ExprAST {
+	private:
+		std::string name_;
+
+	public:
+		VariableExprAST(const std::string& name) : name_(name) {}
+	};
+
+	//演算子の処理のための式クラス
+	//演算子の優先順位は未実装
+	class BinaryExprAST : public ExprAST {
+	private:
+		char op_;
+
+		ExprAST* lhs_, * rhs_;
+
+	public:
+		BinaryExprAST(char op, ExprAST* lhs, ExprAST* rhs) 
+			: op_(op), lhs_(lhs), rhs_(rhs) {}
+	};
+
+	//関数呼び出しのための式クラス
+	class CallExprAST : public ExprAST {
+		std::string callee_;
+		std::vector<ExprAST*> args_;
+	public:
+		CallExprAST(const std::string& callee, std::vector<ExprAST*>& args)
+			: callee_(callee), args_(args) {}
+	};
+
+	//関数のプロトタイプ宣言を表すクラス
+	class PrototypeAST {
+		std::string name_;
+		std::vector<std::string> args_;
+	public:
+		PrototypeAST(const std::string& name, const std::vector<std::string>& args)
+			: name_(name), args_(args) {}
+	};
+
+	//関数定義を表すクラス
+	class FunctionAST {
+		PrototypeAST* proto_;
+		ExprAST* body_;
+	public:
+		FunctionAST(PrototypeAST* proto, ExprAST* body)
+			: proto_(proto), body_(body) {}
+	};
+}
+
